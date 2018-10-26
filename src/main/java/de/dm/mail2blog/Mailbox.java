49590c7c2505774daeb309ad0ccd0568e67dcf5c
Sebastian Tom Gellweiler <sebastian.gellweiler@dm.de>
2018-10-26 15:19:55 +0200

    rewrote Mailbox unit tests to get rid off javamail-mock2-halfmock dependency

--- src/main/java/de/dm/mail2blog/Mailbox.java
+++ src/main/java/de/dm/mail2blog/Mailbox.java
@@ -25,116 +25,119 @@ public class Mailbox implements IMailboxFlagFeature {
     private Folder inbox;
 
     /**
-     * The Mailestore to use.
+     * The Mailstore to use.
      */
     private Store store;
 
     /**
      * Create a new Mailbox
      */
-    public Mailbox(MailConfigurationWrapper mailConfigurationWrapper) throws MailboxException {
+    public Mailbox(MailConfigurationWrapper mailConfigurationWrapper) {
         this.mailConfigurationWrapper = mailConfigurationWrapper;
-
-        // Choose flag strategy based on protocol.
-        if (mailConfigurationWrapper.getMailConfiguration().getProtocol().endsWith("pop3")) {
-            this.flagStrategy = new Pop3MailboxFlagStrategy(this);
-        } else if (mailConfigurationWrapper.getMailConfiguration().getProtocol().endsWith("imap")) {
-            this.flagStrategy = new ImapMailboxFlagStrategy(this);
-        } else {
-            throw new MailboxException("unsupported protocol " + mailConfigurationWrapper.getMailConfiguration().getProtocol());
-        }
-
-        // Connect to mailbox.
-        this.connect();
     }
 
+    private IMailboxFlagFeature getFlagStrategy() throws MailboxException {
+        if (this.flagStrategy == null) {
+            // Choose flag strategy based on protocol.
+            if (mailConfigurationWrapper.getMailConfiguration().getProtocol().endsWith("pop3")) {
+                this.flagStrategy = new Pop3MailboxFlagStrategy(this);
+            } else if (mailConfigurationWrapper.getMailConfiguration().getProtocol().endsWith("imap")) {
+                this.flagStrategy = new ImapMailboxFlagStrategy(this);
+            } else {
+                throw new MailboxException("unsupported protocol " + mailConfigurationWrapper.getMailConfiguration().getProtocol());
+            }
+        }
+        return this.flagStrategy;
+    }
 
     /**
      * Connect to the mail server and log in.
      */
-    private void connect() throws MailboxException {
-        // sanity check.
-        if (
-            StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getServer())
-            || StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getUsername())
-            || mailConfigurationWrapper.getMailConfiguration().getPassword() == null
-        ) {
-            throw new MailboxException("incomplete mail configuration settings (at least one setting is null/empty)");
+    public Store getStore() throws MailboxException {
+        if (this.store == null) {
+            // sanity check.
+            if (
+                    StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getServer())
+                            || StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getUsername())
+                            || mailConfigurationWrapper.getMailConfiguration().getPassword() == null
+            ) {
+                throw new MailboxException("incomplete mail configuration settings (at least one setting is null/empty)");
+            }
+
+            // Create the properties for the session.
+            Properties prop = new Properties();
+
+            // Get the protocol to use.
+            String protocol = mailConfigurationWrapper.getMailConfiguration().getProtocol();
+
+            // Append s to protocol if we use the secure version.
+            if (mailConfigurationWrapper.getMailConfiguration().getSecure()) {
+                protocol += "s";
+            }
+
+            // Assemble the property prefix for this protocol.
+            String propertyPrefix = "mail." + protocol;
+
+            // Get the server port from the configuration and add it to the properties,
+            // but only if it is actually set. If port = 0 this means we use the standard
+            // port for the chosen protocol.
+            if (mailConfigurationWrapper.getMailConfiguration().getPort() != 0) {
+                prop.setProperty(propertyPrefix + ".port", "" + mailConfigurationWrapper.getMailConfiguration().getPort());
+            }
+
+            if (
+                    mailConfigurationWrapper.getMailConfiguration().getSecure()
+                            && mailConfigurationWrapper.getMailConfiguration().getCheckCertificates()
+            ) {
+                prop.setProperty(propertyPrefix + ".ssl.checkserveridentity", "true");
+            } else {
+                prop.setProperty(propertyPrefix + ".ssl.trust", "*");
+                prop.setProperty(propertyPrefix + ".ssl.checkserveridentity", "false");
+            }
+
+            if (!StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getSslVersions())) {
+                prop.setProperty(propertyPrefix + ".ssl.protocols", mailConfigurationWrapper.getMailConfiguration().getSslVersions());
+            }
+
+            // Set connection timeout (10 seconds).
+            prop.setProperty(propertyPrefix + ".connectiontimeout", "10000");
+
+            // Get the session for connecting to the mail server.
+            Session session = getSessionInstance(prop, null);
+
+            // Get the mail store, using the desired protocol.
+            try {
+                this.store = session.getStore(protocol);
+            } catch (NoSuchProviderException e) {
+                throw new MailboxException("no javax.mail provider for protocol " + protocol + ", please change the protocol", e);
+            }
+
+            // Connect to the mailstore.
+            try {
+                this.store.connect(
+                        mailConfigurationWrapper.getMailConfiguration().getServer(),
+                        mailConfigurationWrapper.getMailConfiguration().getUsername(),
+                        mailConfigurationWrapper.getMailConfiguration().getPassword()
+                );
+            } catch (AuthenticationFailedException afe) {
+                throw new MailboxException("authentication for mail store failed", afe);
+            } catch (MessagingException me) {
+                throw new MailboxException("connecting to mail store failed", me);
+            } catch (IllegalStateException ise) {
+                throw new MailboxException("connecting to mail store failed", ise);
+            } catch (Exception e) {
+                throw new MailboxException("connecting to mail store failed", e);
+            }
         }
 
-        // Create the properties for the session.
-        Properties prop = new Properties();
+        return store;
+    }
 
-        // Get the protocol to use.
-        String protocol = mailConfigurationWrapper.getMailConfiguration().getProtocol();
-
-        // Append s to protocol if we use the secure version.
-        if (mailConfigurationWrapper.getMailConfiguration().getSecure()) {
-            protocol += "s";
-        }
-
-        // Assemble the property prefix for this protocol.
-        String propertyPrefix = "mail." + protocol;
-
-        // Get the server port from the configuration and add it to the properties,
-        // but only if it is actually set. If port = 0 this means we use the standard
-        // port for the chosen protocol.
-        if (mailConfigurationWrapper.getMailConfiguration().getPort() != 0)
-        {
-            prop.setProperty(propertyPrefix + ".port", "" + mailConfigurationWrapper.getMailConfiguration().getPort());
-        }
-
-        if (
-            mailConfigurationWrapper.getMailConfiguration().getSecure()
-            && mailConfigurationWrapper.getMailConfiguration().getCheckCertificates()
-        ) {
-            prop.setProperty(propertyPrefix + ".ssl.checkserveridentity", "true");
-        } else {
-            prop.setProperty(propertyPrefix + ".ssl.trust", "*");
-            prop.setProperty(propertyPrefix + ".ssl.checkserveridentity", "false");
-        }
-
-        if (!StringUtils.isBlank(mailConfigurationWrapper.getMailConfiguration().getSslVersions())) {
-            prop.setProperty(propertyPrefix + ".ssl.protocols", mailConfigurationWrapper.getMailConfiguration().getSslVersions());
-        }
-
-        // Set connection timeout (10 seconds).
-        prop.setProperty(propertyPrefix + ".connectiontimeout", "10000");
-
-        // Get the session for connecting to the mail server.
-        Session session = Session.getInstance(prop, null);
-
-        // Get the mail store, using the desired protocol.
-        try {
-            store = session.getStore(protocol);
-        } catch (NoSuchProviderException e) {
-            throw new MailboxException("no javax.mail provider for protocol " + protocol + ", please change the protocol", e);
-        }
-
-        // Connect to the mailstore.
-        try {
-            this.store.connect(
-                mailConfigurationWrapper.getMailConfiguration().getServer(),
-                mailConfigurationWrapper.getMailConfiguration().getUsername(),
-                mailConfigurationWrapper.getMailConfiguration().getPassword()
-            );
-        }
-        catch (AuthenticationFailedException afe)
-        {
-            throw new MailboxException("authentication for mail store failed", afe);
-        }
-        catch (MessagingException me)
-        {
-            throw new MailboxException("connecting to mail store failed", me);
-        }
-        catch (IllegalStateException ise)
-        {
-            throw new MailboxException("connecting to mail store failed", ise);
-        }
-        catch (Exception e)
-        {
-            throw new MailboxException("connecting to mail store failed", e);
-        }
+    /**
+     * Wrapper around javax.mail.Session.getInstance()
+     */
+    public Session getSessionInstance(Properties props, Authenticator authenticator) {
+        return Session.getInstance(props, null);
     }
 
     /**
@@ -144,7 +147,7 @@ public class Mailbox implements IMailboxFlagFeature {
         if (inbox == null || !inbox.isOpen()) {
             try {
                 // Get the INBOX folder.
-                inbox = this.store.getFolder("INBOX");
+                inbox = getStore().getFolder("INBOX");
 
                 // We need to open it READ_WRITE.
                 // because we want to move/delete messages we already handled.
@@ -190,10 +193,12 @@ public class Mailbox implements IMailboxFlagFeature {
             }
         }
 
-        try {
-            store.close();
-        } catch (MessagingException e) {
-            throw new MailboxException("failed to close mail store", e);
+        if (store != null) {
+            try {
+                store.close();
+            } catch (MessagingException e) {
+                throw new MailboxException("failed to close mail store", e);
+            }
         }
     }
 
@@ -201,13 +206,13 @@ public class Mailbox implements IMailboxFlagFeature {
      * Mark message as processed.
      */
     public void flagAsProcessed(Message message) throws MailboxException {
-        this.flagStrategy.flagAsProcessed(message);
+        getFlagStrategy().flagAsProcessed(message);
     }
 
     /**
      * Mark message as invalid.
      */
     public void flagAsInvalid(Message message) throws MailboxException {
-        this.flagStrategy.flagAsInvalid(message);
+        getFlagStrategy().flagAsInvalid(message);
     }
 }
--- src/test/java/ut/de/dm/mail2blog/MailboxTest.java
+++ src/test/java/ut/de/dm/mail2blog/MailboxTest.java
@@ -3,61 +3,126 @@ package ut.de.dm.mail2blog;
 import de.dm.mail2blog.MailConfiguration;
 import de.dm.mail2blog.MailConfigurationWrapper;
 import de.dm.mail2blog.Mailbox;
-import de.saly.javamail.mock2.MailboxFolder;
-import de.saly.javamail.mock2.MockMailbox;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.mail.Authenticator;
+import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
-import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MailboxTest {
 
     MimeMessage exampleMail1;
     MimeMessage exampleMail2;
-    MockMailbox mockMailbox;
+    Store mockStore;
+    Folder mockInbox;
 
     @Before
     public void setUp() throws Exception
     {
-        MockMailbox.resetAll();
-
-        exampleMail1 = new MimeMessage((Session)null);
+        exampleMail1 = spy(new MimeMessage((Session)null));
         exampleMail1.setSubject("Mail1");
         exampleMail1.setFrom(new InternetAddress("alice@example.org"));
         exampleMail1.setText("Content1");
         exampleMail1.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("bob@example.org"));
 
-        exampleMail2 = new MimeMessage((Session)null);
+        exampleMail2 = spy(new MimeMessage((Session)null));
         exampleMail2.setSubject("Mail2");
         exampleMail2.setFrom(new InternetAddress("alice@example.com"));
         exampleMail2.setText("Content2");
         exampleMail2.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("bob@example.org"));
 
-        mockMailbox = MockMailbox.get("bob@example.org");
-        MailboxFolder inbox = mockMailbox.getInbox();
-        inbox.add(exampleMail1);
-        inbox.add(exampleMail2);
+        mockStore = mock(Store.class);
+
+        mockInbox = mock(Folder.class);
+        when(mockStore.getFolder("INBOX")).thenReturn(mockInbox);
+        when(mockInbox.getMessageCount()).thenReturn(2);
+        when(mockInbox.getMessages()).thenReturn(new Message[]{exampleMail1, exampleMail2});
+
+        when(exampleMail1.getFolder()).thenReturn(mockInbox);
+        when(exampleMail2.getFolder()).thenReturn(mockInbox);
     }
 
-    private MailConfiguration.MailConfigurationBuilder getBasicConfiguration() {
-        return MailConfiguration.builder()
+    @Test
+    public void testGetStoreImap() throws Exception {
+        MailConfiguration mailConfiguration = MailConfiguration.builder()
+        .server("mail.example.org")
+        .secure(true)
+        .sslVersions("TLSv1.2")
+        .checkCertificates(true)
+        .username("bob")
+        .password("password")
+        .protocol("imap")
+        .port(143)
+        .emailaddress("bob@example.org")
+        .build();
+
+        Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(mailConfiguration)));
+        final ArgumentCaptor<Properties> propertiesCaptor = ArgumentCaptor.forClass(Properties.class);
+
+        Session session = mock(Session.class);
+        doReturn(session).when(mailbox).getSessionInstance(propertiesCaptor.capture(), (Authenticator)isNull());
+        when(session.getStore("imaps")).thenReturn(mockStore);
+
+        mailbox.getStore();
+
+        verify(mockStore, times(1)).connect("mail.example.org", "bob", "password");
+        Properties properties = propertiesCaptor.getValue();
+        assertEquals("10000", properties.getProperty("mail.imaps.connectiontimeout"));
+        assertEquals("143", properties.getProperty("mail.imaps.port"));
+        assertEquals("true", properties.getProperty("mail.imaps.ssl.checkserveridentity"));
+        assertNull(properties.getProperty("mail.pop3.ssl.trust"));
+        assertEquals("TLSv1.2", properties.getProperty("mail.imaps.ssl.protocols"));
+    }
+
+    @Test
+    public void testGetStorePop3() throws Exception {
+        MailConfiguration mailConfiguration = MailConfiguration.builder()
         .server("mail.example.org")
         .secure(false)
-        .username("bob@example.org")
+        .username("bob")
         .password("password")
-        .protocol("mock_pop3")
-        .port(110)
-        .emailaddress("bob@example.org");
+        .protocol("pop3")
+        .port(1110)
+        .emailaddress("bob@example.org")
+        .build();
+
+        Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(mailConfiguration)));
+        final ArgumentCaptor<Properties> propertiesCaptor = ArgumentCaptor.forClass(Properties.class);
+
+        Session session = mock(Session.class);
+        doReturn(session).when(mailbox).getSessionInstance(propertiesCaptor.capture(), (Authenticator)isNull());
+        when(session.getStore("pop3")).thenReturn(mockStore);
+
+        mailbox.getStore();
+
+        verify(mockStore, times(1)).connect("mail.example.org", "bob", "password");
+        Properties properties = propertiesCaptor.getValue();
+        assertEquals("10000", properties.getProperty("mail.pop3.connectiontimeout"));
+        assertEquals("1110", properties.getProperty("mail.pop3.port"));
+        assertEquals("false", properties.getProperty("mail.pop3.ssl.checkserveridentity"));
+        assertEquals("*", properties.getProperty("mail.pop3.ssl.trust"));
     }
 
     /**
@@ -66,97 +131,86 @@ public class MailboxTest {
     @Test
     public void testCount() throws Exception
     {
-        MailConfiguration mailConfiguration = getBasicConfiguration().build();
-
-        Mailbox mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
-
+        Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(MailConfiguration.builder().build())));
+        doReturn(mockStore).when(mailbox).getStore();
         assertEquals("Expected 2 messages in INBOX", 2, mailbox.getCount());
     }
 
-    /**
-     * Check that the number of messages in the inbox are correctly counted.
-     */
-    @Test
-    public void testGetMessages() throws Exception
-    {
-        MailConfiguration mailConfiguration = getBasicConfiguration().build();
+   /**
+    * Check that the number of messages in the inbox are correctly counted.
+    */
+   @Test
+   public void testGetMessages() throws Exception
+   {
+       Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(MailConfiguration.builder().build())));
+       doReturn(mockStore).when(mailbox).getStore();
 
-        Mailbox mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
+       Message[] messages = mailbox.getMessages();
+       assertEquals("Expected 2 messages in INBOX", 2, messages.length);
+       assertEquals("Failed to get first message from INBOX", exampleMail1.getSubject(), messages[0].getSubject());
+       assertEquals("Failed to get second message from INBOX", exampleMail2.getSubject(), messages[1].getSubject());
+   }
 
-        Message[] messages = mailbox.getMessages();
-        assertEquals("Expected 2 messages in INBOX", 2, messages.length);
-        assertEquals("Failed to get first message from INBOX", exampleMail1.getSubject(), messages[0].getSubject());
-        assertEquals("Failed to get second message from INBOX", exampleMail2.getSubject(), messages[1].getSubject());
-    }
+   /**
+    * Test flagging on IMAP.
+    */
+   @Test
+   public void testFlaggingImap() throws Exception
+   {
+       Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(
+           MailConfiguration.builder()
+               .protocol("imap")
+               .build()
+           ))
+       );
+       doReturn(mockStore).when(mailbox).getStore();
 
-    /**
-     * Test flagging on IMAP.
-     */
-    @Test
-    public void testFlaggingImap() throws Exception
-    {
-        MailConfiguration mailConfiguration = getBasicConfiguration()
-        .protocol("mock_imap")
-        .port(143)
-        .build();
+       Folder processed = mock(Folder.class);
+       Folder invalid = mock(Folder.class);
 
-        Mailbox mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
+       when(mockInbox.getFolder("Processed")).thenReturn(processed);
+       when(mockInbox.getFolder("Invalid")).thenReturn(invalid);
 
-        Message[] messages = mailbox.getMessages();
-        assertEquals("Expected 2 messages in INBOX", 2, messages.length);
+       when(processed.exists()).thenReturn(false);
+       when(invalid.exists()).thenReturn(true);
+       when(processed.create(Folder.HOLDS_MESSAGES)).thenReturn(true);
 
-        // Flag messages.
-        mailbox.flagAsProcessed(messages[0]);
-        mailbox.flagAsInvalid(messages[1]);
+       processed.open(Folder.READ_ONLY);
+       invalid.open(Folder.READ_ONLY);
 
-        Folder processed = mailbox.getInbox().getFolder("Processed");
-        Folder invalid = mailbox.getInbox().getFolder("Invalid");
+       // Flag messages.
+       mailbox.flagAsProcessed(exampleMail1);
+       mailbox.flagAsInvalid(exampleMail2);
 
-        assertNotNull("Failed to get processed folder", processed);
-        assertNotNull("Failed to get invalid folder", invalid);
+       verify(processed, times(1)).create(Folder.HOLDS_MESSAGES);
+       verify(invalid, times(0)).create(anyInt());
+       verify(processed, times(1)).open(Folder.READ_WRITE);
+       verify(invalid, times(1)).open(Folder.READ_WRITE);
+       verify(mockInbox, times(1)).copyMessages(new Message[]{exampleMail1}, processed);
+       verify(mockInbox, times(0)).copyMessages(new Message[]{exampleMail1}, invalid);
+       verify(mockInbox, times(1)).copyMessages(new Message[]{exampleMail2}, invalid);
+       verify(mockInbox, times(0)).copyMessages(new Message[]{exampleMail2}, processed);
+   }
 
-        processed.open(Folder.READ_ONLY);
-        invalid.open(Folder.READ_ONLY);
+   /**
+    * Test flagging on Pop3.
+    */
+   @Test
+   public void testFlaggingPop3() throws Exception
+   {
+       Mailbox mailbox = spy(new Mailbox(new MailConfigurationWrapper(
+           MailConfiguration.builder()
+               .protocol("pop3")
+               .build()
+           ))
+       );
 
-        Message processedMessage = processed.getMessage(1);
-        Message invalidMessage = invalid.getMessage(1);
+       // Flag messages.
+       mailbox.flagAsProcessed(exampleMail1);
+       mailbox.flagAsInvalid(exampleMail2);
 
-        assertNotNull("No message in processed folder", processedMessage);
-        assertNotNull("No message in invalid", invalidMessage);
-
-        assertEquals("Failed to mark message as processed", exampleMail1.getSubject(), processedMessage.getSubject());
-        assertEquals("Failed to mark message as invalid", exampleMail2.getSubject(), invalidMessage.getSubject());
-
-        mailbox.close();
-
-        mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
-        assertEquals("Expected 0 messages in INBOX", 0, mailbox.getCount());
-    }
-
-    /**
-     * Test flagging on Pop3.
-     */
-    @Test
-    public void testFlaggingPop3() throws Exception
-    {
-        MailConfiguration mailConfiguration = getBasicConfiguration()
-        .protocol("mock_pop3")
-        .port(110)
-        .build();
-
-        Mailbox mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
-
-        Message[] messages = mailbox.getMessages();
-        assertEquals("Expected 2 messages in INBOX", 2, messages.length);
-
-        // Flag messages.
-        mailbox.flagAsProcessed(messages[0]);
-        mailbox.flagAsInvalid(messages[1]);
-
-        mailbox.close();
-
-        mailbox = new Mailbox(new MailConfigurationWrapper(mailConfiguration));
-        assertEquals("Expected 0 messages in INBOX", 0, mailbox.getCount());
-    }
+       verify(exampleMail1, times(1)).setFlag(Flags.Flag.DELETED, true);
+       verify(exampleMail2, times(1)).setFlag(Flags.Flag.DELETED, true);
+   }
 }
 
